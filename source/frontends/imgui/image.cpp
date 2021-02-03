2c0fc9887e78be0f7176278d71866373741c06fc
Andrea Odetti <mariofutire@gmail.com>
2021-02-03 15:16:50 +0000

    Better handling of GLES2/3
    Signed-off-by: Andrea Odetti <mariofutire@gmail.com>

--- source/frontends/imgui/image.cpp
+++ source/frontends/imgui/image.cpp
@@ -1,8 +1,11 @@
 #include "frontends/imgui/image.h"
+#include <GLES2/gl2ext.h>
 
 #ifdef GL_UNPACK_ROW_LENGTH
+// GLES3: defined in gl3.h
 #define UGL_UNPACK_LENGTH GL_UNPACK_ROW_LENGTH
 #else
+// GLES2: defined in gl2ext.h as an extension GL_EXT_unpack_subimage
 #define UGL_UNPACK_LENGTH GL_UNPACK_ROW_LENGTH_EXT
 #endif
 
@@ -10,17 +13,16 @@ void LoadTextureFromData(GLuint texture, const uint8_t * data, size_t width, siz
 {
   glBindTexture(GL_TEXTURE_2D, texture);
   glPixelStorei(UGL_UNPACK_LENGTH, pitch); // in pixels
-  GLint data2;
-  glGetIntegerv(UGL_UNPACK_LENGTH, &data2);
+
   // Setup filtering parameters for display
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-  // I have no idea how this works, but it works
-  const GLenum format = GL_RGBA; // without _EXT colors are all over the place
+  const GLenum format = GL_BGRA_EXT; // this is defined in gl2ext.h and nowhere in gl3.h
   const GLenum type = GL_UNSIGNED_BYTE;
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, format, type, data);
+  glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, type, data);
+  // reset to default state
   glPixelStorei(UGL_UNPACK_LENGTH, 0);
 }
